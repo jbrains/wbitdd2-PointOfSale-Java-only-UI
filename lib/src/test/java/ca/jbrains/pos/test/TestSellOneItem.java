23ca96f4f01ff38213353fc2a6d4f692503d8966
J. B. Rainsberger <me@jbrains.ca>
2021-12-16 18:39:18 -0400

    Improved formatting.

--- lib/src/test/java/ca/jbrains/pos/test/TestSellOneItem.java
+++ lib/src/test/java/ca/jbrains/pos/test/TestSellOneItem.java
@@ -12,29 +12,20 @@ public class TestSellOneItem {
     void priceNotFound() {
         String response = PointOfSale.handleSellOneItemRequest(barcode -> Option.none(), new Barcode("99999"));
 
-        Assertions.assertEquals(
-                "Product not found: 99999",
-                response
-        );
+        Assertions.assertEquals("Product not found: 99999", response);
     }
 
     @Test
     void givenBarcodeIs1111ShouldDisplayProductNotFoundMessage() {
         String response = PointOfSale.handleSellOneItemRequest(barcode -> Option.none(), Barcode.makeBarcode("1111").get());
 
-        Assertions.assertEquals(
-                "Product not found: 1111",
-                response
-        );
+        Assertions.assertEquals("Product not found: 1111", response);
     }
 
     @Test
     void priceFound() {
         String response = PointOfSale.handleSellOneItemRequest(barcode -> Option.of(100), Barcode.makeBarcode("99999").get());
 
-        Assertions.assertEquals(
-                "CAD 1.00",
-                response
-        );
+        Assertions.assertEquals("CAD 1.00", response);
     }
 }
