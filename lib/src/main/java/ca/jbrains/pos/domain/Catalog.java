6459a83d1f547903ef998fe5f25600670911420b
J. B. Rainsberger <me@jbrains.ca>
2021-12-16 18:24:33 -0400

    Identified a refactoring.

--- lib/src/main/java/ca/jbrains/pos/domain/Catalog.java
+++ lib/src/main/java/ca/jbrains/pos/domain/Catalog.java
@@ -4,5 +4,6 @@ import io.vavr.control.Option;
 
 public interface Catalog {
     // CONTRACT Assumes CAD as the currency
+    // REFACTOR Replace String to Barcode
     Option<Integer> findPrice(String barcode);
 }
