6c4cdb80fd8588584ab90dfc6498015261a97d0e
Tony Vo <ttrung.vo@gmail.com>
2021-12-16 18:17:28 -0700

    R!! add default getUnformattedPrice to the interface

---
+++ lib/src/main/java/ca/jbrains/pos/domain/Price.java
@@ -0,0 +1,9 @@
+package ca.jbrains.pos.domain;
+
+public class Price {
+    private final int moneyInCents;
+
+    public Price(int moneyInCents) {
+        this.moneyInCents = moneyInCents;
+    }
+}
