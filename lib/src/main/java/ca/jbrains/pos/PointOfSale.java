48f4af88cf28ceced42f5828a5dfe07be9800474
J. B. Rainsberger <me@jbrains.ca>
2021-12-20 12:27:47 -0400

    Made similar code more similar: replaced fallbackValue with maybe.map(f).getOrElse(fallbackValue).

--- lib/src/main/java/ca/jbrains/pos/PointOfSale.java
+++ lib/src/main/java/ca/jbrains/pos/PointOfSale.java
@@ -19,7 +19,7 @@ public class PointOfSale {
     public static String handleCommand(String commandText) {
         Option<Barcode> parseResult = Barcode.parse(commandText);
         if (parseResult.isEmpty()) {
-            return "Error: empty command";
+            return parseResult.map(barcode -> handleBarcodeScanned(barcode, (ignored) -> "::a hardcoded response for every barcode::")).getOrElse("Error: empty command");
         } else {
             return parseResult.map(barcode -> handleBarcodeScanned(barcode, (ignored) -> "::a hardcoded response for every barcode::")).get();
         }
