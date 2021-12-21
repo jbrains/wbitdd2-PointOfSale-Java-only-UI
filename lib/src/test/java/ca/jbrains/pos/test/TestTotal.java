f32076b9f233e7683ccd55a224bb26c1cae3669b
J. B. Rainsberger <me@jbrains.ca>
2021-12-21 09:23:58 -0400

    Gave names to some anonymous implementations of Basket.

--- lib/src/test/java/ca/jbrains/pos/test/TestTotal.java
+++ lib/src/test/java/ca/jbrains/pos/test/TestTotal.java
@@ -1,38 +1,46 @@
 package ca.jbrains.pos.test;
 
-import ca.jbrains.pos.Barcode;
 import ca.jbrains.pos.PointOfSale;
 import ca.jbrains.pos.domain.Basket;
-import io.vavr.control.Option;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestTotal {
     @Test
     void noItems() {
-        Assertions.assertEquals("Total: CAD 0.00", PointOfSale.handleTotal(new Basket() {
-            @Override
-            public void add(int price) {
-            }
-
-            @Override
-            public int getTotal() {
-                return 0;
-            }
-        }));
+        Assertions.assertEquals("Total: CAD 0.00", PointOfSale.handleTotal(new EmptyBasket()));
     }
 
     @Test
     void oneItem() {
-        Assertions.assertEquals("Total: CAD 1.02", PointOfSale.handleTotal(new Basket() {
-            @Override
-            public void add(int price) {
-            }
+        Assertions.assertEquals("Total: CAD 1.02", PointOfSale.handleTotal(new NotEmptyBasket(102)));
+    }
 
-            @Override
-            public int getTotal() {
-                return 102;
-            }
-        }));
+    private static class EmptyBasket implements Basket {
+        @Override
+        public void add(int price) {
+        }
+
+        @Override
+        public int getTotal() {
+            return 0;
+        }
+    }
+
+    private static class NotEmptyBasket implements Basket {
+        private final int price;
+
+        public NotEmptyBasket(int price) {
+            this.price = price;
+        }
+
+        @Override
+        public void add(int price) {
+        }
+
+        @Override
+        public int getTotal() {
+            return price;
+        }
     }
 }
