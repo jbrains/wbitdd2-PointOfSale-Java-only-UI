57e76cace0723321ad4cc649719d69be0e823063
J. B. Rainsberger <me@jbrains.ca>
2022-01-12 15:39:52 -0400

    Replaced LegacyCatalogAdapter with Catalog stubs in some tests.

--- lib/src/test/java/ca/jbrains/pos/test/TestSellOneItem.java
+++ lib/src/test/java/ca/jbrains/pos/test/TestSellOneItem.java
@@ -1,9 +1,9 @@
 package ca.jbrains.pos.test;
 
 import ca.jbrains.pos.Barcode;
-import ca.jbrains.pos.Catalog;
 import ca.jbrains.pos.PointOfSale;
 import ca.jbrains.pos.domain.Basket;
+import io.vavr.control.Either;
 import io.vavr.control.Option;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -12,25 +12,23 @@ public class TestSellOneItem {
 
     @Test
     void priceNotFound() {
-        String response = PointOfSale.handleSellOneItemRequest(new Barcode("99999"), null, catalogFindingPrice(Option.none()));
+        String response = PointOfSale.handleSellOneItemRequest(new Barcode("99999"), null, barcode -> Either.left(barcode));
 
         Assertions.assertEquals("Product not found: 99999", response);
     }
 
-    private Catalog catalogFindingPrice(Option<Integer> maybeMatchingPrice) {
-        return barcode -> maybeMatchingPrice.toEither(barcode);
-    }
-
     @Test
     void givenBarcodeIs1111ShouldDisplayProductNotFoundMessage() {
-        String response = PointOfSale.handleSellOneItemRequest(Barcode.makeBarcode("1111").get(), null, catalogFindingPrice(Option.none()));
+        Barcode barcode = Barcode.makeBarcode("1111").get();
+
+        String response = PointOfSale.handleSellOneItemRequest(barcode, null, ignored -> Either.left(barcode));
 
         Assertions.assertEquals("Product not found: 1111", response);
     }
 
     @Test
     void priceFound() {
-        String response = PointOfSale.handleSellOneItemRequest(Barcode.makeBarcode("99999").get(), new DoNothingBasket(), catalogFindingPrice(Option.of(100)));
+        String response = PointOfSale.handleSellOneItemRequest(Barcode.makeBarcode("99999").get(), new DoNothingBasket(), barcode -> Either.right(100));
 
         Assertions.assertEquals("CAD 1.00", response);
     }
@@ -42,7 +40,7 @@ public class TestSellOneItem {
     void addItemToBasketWhenProductIsFound() {
         Basket basket = new RecordingBasket();
 
-        PointOfSale.handleSellOneItemRequest(Barcode.makeBarcode("::any barcode::").get(), basket, catalogFindingPrice(Option.some(100)));
+        PointOfSale.handleSellOneItemRequest(Barcode.makeBarcode("::any barcode::").get(), basket, barcode -> Either.right(100));
         Assertions.assertEquals(Option.some(100), addInvokedWith);
     }
 
