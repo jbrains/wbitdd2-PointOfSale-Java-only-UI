039a6dbaf9061119d305cf8edc809f680cb6c668
J. B. Rainsberger <me@jbrains.ca>
2022-01-12 12:59:47 -0400

    Rename the outgoing interface, so that the incoming interface can replace it.

--- lib/src/test/java/ca/jbrains/pos/test/PurchaseTest.java
+++ lib/src/test/java/ca/jbrains/pos/test/PurchaseTest.java
@@ -2,7 +2,7 @@ package ca.jbrains.pos.test;
 
 import ca.jbrains.pos.PointOfSale;
 import ca.jbrains.pos.domain.Basket;
-import ca.jbrains.pos.domain.Catalog;
+import ca.jbrains.pos.domain.LegacyCatalog;
 import io.vavr.control.Option;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -13,23 +13,23 @@ import java.util.stream.Collectors;
 public class PurchaseTest {
     @Test
     void oneItem() {
-        Catalog catalog = ignored -> Option.of(795);
+        LegacyCatalog legacyCatalog = ignored -> Option.of(795);
         Basket basket = new NotEmptyBasket(795);
 
         // SMELL Duplicates logic in PointOfSale.runApplication(): stream lines, handle each line, consume the result
         Assertions.assertEquals(
                 List.of("CAD 7.95", "Total: CAD 7.95"),
-                List.of("12345", "total").stream().map(line -> PointOfSale.handleLine(line, catalog, basket)).collect(Collectors.toList()));
+                List.of("12345", "total").stream().map(line -> PointOfSale.handleLine(line, legacyCatalog, basket)).collect(Collectors.toList()));
     }
 
     @Test
     void aDifferentItem() {
-        Catalog catalog = ignored -> Option.of(995);
+        LegacyCatalog legacyCatalog = ignored -> Option.of(995);
         Basket basket = new NotEmptyBasket(995);
 
         // SMELL Duplicates logic in PointOfSale.runApplication(): stream lines, handle each line, consume the result
         Assertions.assertEquals(
                 List.of("CAD 9.95", "Total: CAD 9.95"),
-                List.of("12345", "total").stream().map(line -> PointOfSale.handleLine(line, catalog, basket)).collect(Collectors.toList()));
+                List.of("12345", "total").stream().map(line -> PointOfSale.handleLine(line, legacyCatalog, basket)).collect(Collectors.toList()));
     }
 }
