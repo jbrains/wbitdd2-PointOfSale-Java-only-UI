9081d7b3eed174eb2ec75ce611136e5a217d9cd0
J. B. Rainsberger <me@jbrains.ca>
2022-01-13 17:58:37 -0400

    Moved function onto Catalog, but it's static.

--- lib/src/main/java/ca/jbrains/pos/domain/Catalog.java
+++ lib/src/main/java/ca/jbrains/pos/domain/Catalog.java
@@ -1,9 +1,15 @@
 package ca.jbrains.pos.domain;
 
 import ca.jbrains.pos.Barcode;
+import io.vavr.control.Either;
 import io.vavr.control.Option;
 
 public interface Catalog {
+    // REFACTOR Move into The Hole onto Catalog
+    static Either<Barcode, Integer> findProductInCatalog(Barcode barcode, Catalog catalog) {
+        return catalog.findPrice(barcode).toEither(barcode);
+    }
+
     // CONTRACT Assumes CAD as the currency
     Option<Integer> findPrice(Barcode barcode);
 }
