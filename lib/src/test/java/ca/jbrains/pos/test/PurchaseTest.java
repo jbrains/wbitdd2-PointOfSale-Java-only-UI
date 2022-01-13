4b7bcfff8ccf549d1a0895757060c17bba73c8be
J. B. Rainsberger <me@jbrains.ca>
2022-01-13 19:33:42 -0400

    One test now stubs Catalog directly, instead of stubbing LegacyCatalog.

--- lib/src/test/java/ca/jbrains/pos/test/PurchaseTest.java
+++ lib/src/test/java/ca/jbrains/pos/test/PurchaseTest.java
@@ -1,9 +1,11 @@
 package ca.jbrains.pos.test;
 
+import ca.jbrains.pos.Catalog;
 import ca.jbrains.pos.LegacyCatalogAdapter;
 import ca.jbrains.pos.PointOfSale;
 import ca.jbrains.pos.domain.Basket;
 import ca.jbrains.pos.domain.LegacyCatalog;
+import io.vavr.control.Either;
 import io.vavr.control.Option;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -17,13 +19,16 @@ public class PurchaseTest {
     void oneItem() {
         LegacyCatalog legacyCatalog = Mockito.mock(LegacyCatalog.class);
         Mockito.when(legacyCatalog.findPrice(Mockito.any())).thenReturn(Option.of(795));
+        LegacyCatalogAdapter legacyCatalogAdapter = new LegacyCatalogAdapter(legacyCatalog);
+        Catalog catalog = Mockito.mock(Catalog.class);
+        Mockito.when(catalog.findPrice(Mockito.any())).thenReturn(Either.right(795));
 
         Basket basket = new NotEmptyBasket(795);
 
         // SMELL Duplicates logic in PointOfSale.runApplication(): stream lines, handle each line, consume the result
         Assertions.assertEquals(
                 List.of("CAD 7.95", "Total: CAD 7.95"),
-                List.of("12345", "total").stream().map(line -> PointOfSale.handleLine(line, basket, new LegacyCatalogAdapter(legacyCatalog))).collect(Collectors.toList()));
+                List.of("12345", "total").stream().map(line -> PointOfSale.handleLine(line, basket, catalog)).collect(Collectors.toList()));
     }
 
     @Test
