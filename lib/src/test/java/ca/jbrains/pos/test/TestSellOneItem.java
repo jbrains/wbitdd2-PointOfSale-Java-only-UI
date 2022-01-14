22ab1fc6754eef0631985fe8bd03040db6e67c20
J. B. Rainsberger <me@jbrains.ca>
2022-01-14 19:12:08 -0400

    Replaced LegacyCatalog stubs with Catalog stubs in the remaining tests.

--- lib/src/test/java/ca/jbrains/pos/test/TestSellOneItem.java
+++ lib/src/test/java/ca/jbrains/pos/test/TestSellOneItem.java
@@ -1,10 +1,10 @@
 package ca.jbrains.pos.test;
 
 import ca.jbrains.pos.Barcode;
-import ca.jbrains.pos.LegacyCatalogAdapter;
 import ca.jbrains.pos.PointOfSale;
 import ca.jbrains.pos.domain.Basket;
-import ca.jbrains.pos.domain.LegacyCatalog;
+import ca.jbrains.pos.domain.Catalog;
+import io.vavr.control.Either;
 import io.vavr.control.Option;
 import org.jmock.Expectations;
 import org.jmock.junit5.JUnit5Mockery;
@@ -15,46 +15,46 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 public class TestSellOneItem {
     @RegisterExtension
     final JUnit5Mockery context = new JUnit5Mockery();
-    private final LegacyCatalog legacyCatalog = context.mock(LegacyCatalog.class);
+    private final Catalog catalog = context.mock(Catalog.class);
 
     @Test
     void priceNotFound() {
-        neverFindPrice(legacyCatalog);
+        neverFindPrice(catalog, new Barcode("99999"));
 
-        String response = PointOfSale.handleSellOneItemRequest(new Barcode("99999"), null, new LegacyCatalogAdapter(legacyCatalog));
+        String response = PointOfSale.handleSellOneItemRequest(new Barcode("99999"), null, catalog);
 
         Assertions.assertEquals("Product not found: 99999", response);
     }
 
-    private void neverFindPrice(final LegacyCatalog legacyCatalog) {
+    private void neverFindPrice(final Catalog catalog, Barcode missingBarcode) {
         context.checking(new Expectations() {{
-            allowing(legacyCatalog).findPrice(with(aNonNull(Barcode.class)));
-            will(returnValue(Option.none()));
+            allowing(catalog).findPrice(missingBarcode);
+            will(returnValue(Either.left(missingBarcode)));
         }});
     }
 
     @Test
     void givenBarcodeIs1111ShouldDisplayProductNotFoundMessage() {
-        neverFindPrice(legacyCatalog);
+        neverFindPrice(catalog, new Barcode("1111"));
 
-        String response = PointOfSale.handleSellOneItemRequest(Barcode.makeBarcode("1111").get(), null, new LegacyCatalogAdapter(legacyCatalog));
+        String response = PointOfSale.handleSellOneItemRequest(Barcode.makeBarcode("1111").get(), null, catalog);
 
         Assertions.assertEquals("Product not found: 1111", response);
     }
 
     @Test
     void priceFound() {
-        alwaysFindPrice(legacyCatalog, 100);
+        alwaysFindPrice(catalog, 100);
 
-        String response = PointOfSale.handleSellOneItemRequest(Barcode.makeBarcode("99999").get(), new DoNothingBasket(), new LegacyCatalogAdapter(legacyCatalog));
+        String response = PointOfSale.handleSellOneItemRequest(Barcode.makeBarcode("99999").get(), new DoNothingBasket(), catalog);
 
         Assertions.assertEquals("CAD 1.00", response);
     }
 
-    private void alwaysFindPrice(final LegacyCatalog legacyCatalog, final int matchingPrice) {
+    private void alwaysFindPrice(final Catalog catalog, final int matchingPrice) {
         context.checking(new Expectations() {{
-            allowing(legacyCatalog).findPrice(with(aNonNull(Barcode.class)));
-            will(returnValue(Option.of(matchingPrice)));
+            allowing(catalog).findPrice(with(aNonNull(Barcode.class)));
+            will(returnValue(Either.right(matchingPrice)));
         }});
     }
 
@@ -63,10 +63,10 @@ public class TestSellOneItem {
 
     @Test
     void addItemToBasketWhenProductIsFound() {
-        alwaysFindPrice(legacyCatalog, 100);
+        alwaysFindPrice(catalog, 100);
         Basket basket = new RecordingBasket();
 
-        PointOfSale.handleSellOneItemRequest(Barcode.makeBarcode("::any barcode::").get(), basket, new LegacyCatalogAdapter(legacyCatalog));
+        PointOfSale.handleSellOneItemRequest(Barcode.makeBarcode("::any barcode::").get(), basket, catalog);
         Assertions.assertEquals(Option.some(100), addInvokedWith);
     }
 
