b1e2b58a4b49f49610f5cd56dcea8979e42987c1
Tony Vo <ttrung.vo@gmail.com>
2022-01-15 11:50:29 -0700

    r replace lambda with anonymous class in catalogfactory

--- lib/src/test/java/ca/jbrains/pos/test/CatalogFactory.java
+++ lib/src/test/java/ca/jbrains/pos/test/CatalogFactory.java
@@ -1,14 +1,25 @@
 package ca.jbrains.pos.test;
 
+import ca.jbrains.pos.Barcode;
 import ca.jbrains.pos.domain.Catalog;
 import io.vavr.control.Option;
 
 public class CatalogFactory {
     static Catalog catalogWithPrice(int price) {
-        return barcode -> Option.of(price);
+        return new Catalog() {
+            @Override
+            public Option<Integer> findPrice(Barcode barcode) {
+                return Option.of(price);
+            }
+        };
     }
 
     static Catalog priceNotFoundCatalog() {
-        return barcode -> Option.none();
+        return new Catalog() {
+            @Override
+            public Option<Integer> findPrice(Barcode barcode) {
+                return Option.none();
+            }
+        };
     }
 }
