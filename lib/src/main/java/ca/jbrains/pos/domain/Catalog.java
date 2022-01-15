c1ea2277eaf9558a9b144478737c1ab833d43a47
Tony Vo <ttrung.vo@gmail.com>
2022-01-15 11:52:01 -0700

    r push findPrice and findProductInCatalog down

--- lib/src/main/java/ca/jbrains/pos/domain/Catalog.java
+++ lib/src/main/java/ca/jbrains/pos/domain/Catalog.java
@@ -2,14 +2,8 @@ package ca.jbrains.pos.domain;
 
 import ca.jbrains.pos.Barcode;
 import io.vavr.control.Either;
-import io.vavr.control.Option;
 
 public interface Catalog {
     // REFACTOR Move into The Hole onto Catalog
-    default Either<Barcode, Integer> findProductInCatalog(Barcode barcode) {
-        return findPrice(barcode).toEither(barcode);
-    }
-
-    // CONTRACT Assumes CAD as the currency
-    Option<Integer> findPrice(Barcode barcode);
+    Either<Barcode, Integer> findProductInCatalog(Barcode barcode);
 }
--- lib/src/test/java/ca/jbrains/pos/test/CatalogFactory.java
+++ lib/src/test/java/ca/jbrains/pos/test/CatalogFactory.java
@@ -2,12 +2,18 @@ package ca.jbrains.pos.test;
 
 import ca.jbrains.pos.Barcode;
 import ca.jbrains.pos.domain.Catalog;
+import io.vavr.control.Either;
 import io.vavr.control.Option;
 
 public class CatalogFactory {
     static Catalog catalogWithPrice(int price) {
         return new Catalog() {
+            // REFACTOR Move into The Hole onto Catalog
             @Override
+            public Either<Barcode, Integer> findProductInCatalog(Barcode barcode) {
+                return findPrice(barcode).toEither(barcode);
+            }
+
             public Option<Integer> findPrice(Barcode barcode) {
                 return Option.of(price);
             }
@@ -16,7 +22,12 @@ public class CatalogFactory {
 
     static Catalog priceNotFoundCatalog() {
         return new Catalog() {
+            // REFACTOR Move into The Hole onto Catalog
             @Override
+            public Either<Barcode, Integer> findProductInCatalog(Barcode barcode) {
+                return findPrice(barcode).toEither(barcode);
+            }
+
             public Option<Integer> findPrice(Barcode barcode) {
                 return Option.none();
             }
