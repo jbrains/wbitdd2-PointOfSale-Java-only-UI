f7d1676f52e2a487e7fab55c0289ea89c03dba57
Tony Vo <ttrung.vo@gmail.com>
2022-01-15 14:50:01 -0700

    R replace stubs with mockito in CatalogFactory

--- lib/src/test/java/ca/jbrains/pos/test/CatalogFactory.java
+++ lib/src/test/java/ca/jbrains/pos/test/CatalogFactory.java
@@ -1,18 +1,30 @@
 package ca.jbrains.pos.test;
 
+import ca.jbrains.pos.Barcode;
 import ca.jbrains.pos.Catalog;
 import ca.jbrains.pos.PointOfSale;
 import ca.jbrains.pos.domain.LegacyCatalog;
+import io.vavr.control.Either;
 import io.vavr.control.Option;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class CatalogFactory {
     static Catalog catalogWithPrice(int value) {
-        LegacyCatalog legacyCatalog = ignored -> Option.of(value);
-        Catalog catalog = new PointOfSale.LegacyCatalogAdapter(legacyCatalog);
+        Catalog catalog = mock(Catalog.class);
+        when(catalog.findPrice(any()))
+                .thenReturn(Either.right(value));
         return catalog;
     }
 
-    static PointOfSale.LegacyCatalogAdapter priceNotFoundCatalog() {
-        return new PointOfSale.LegacyCatalogAdapter(barcode -> Option.none());
+    static Catalog priceNotFoundCatalog() {
+        Catalog catalog = mock(Catalog.class);
+        when(catalog.findPrice(any()))
+                .thenAnswer((Answer<Either<Barcode, Integer>>) invocation -> Either.left(invocation.getArgument(0)));
+        return catalog;
     }
 }
