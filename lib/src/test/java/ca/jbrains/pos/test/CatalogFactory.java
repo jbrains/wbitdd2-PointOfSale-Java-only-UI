910012d561d9bfd39ee49fe99e236b1f253c68c4
Tony Vo <ttrung.vo@gmail.com>
2022-01-15 13:53:20 -0700

    R use mockito for CatalogFactory

--- lib/src/test/java/ca/jbrains/pos/test/CatalogFactory.java
+++ lib/src/test/java/ca/jbrains/pos/test/CatalogFactory.java
@@ -3,12 +3,20 @@ package ca.jbrains.pos.test;
 import ca.jbrains.pos.domain.Catalog;
 import io.vavr.control.Option;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
 public class CatalogFactory {
     static Catalog priceNotFoundCatalog() {
-        return barcode -> Option.none();
+        Catalog catalog = spy(Catalog.class);
+        when(catalog.findPrice(any())).thenReturn(Option.none());
+        return catalog;
     }
 
     static Catalog catalogWithPrice(int value) {
-        return barcode -> Option.of(value);
+        Catalog catalog = spy(Catalog.class);
+        when(catalog.findPrice(any())).thenReturn(Option.of(value));
+        return catalog;
     }
 }
