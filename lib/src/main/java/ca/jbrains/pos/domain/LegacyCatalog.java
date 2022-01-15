c72d1470b72b6a0254721943fd4c88f8931ac1ff
Tony Vo <ttrung.vo@gmail.com>
2022-01-15 11:27:30 -0700

    r remove unused LegacyCatalog

--- lib/src/main/java/ca/jbrains/pos/domain/LegacyCatalog.java
+++
@@ -1,9 +0,0 @@
-package ca.jbrains.pos.domain;
-
-import ca.jbrains.pos.Barcode;
-import io.vavr.control.Option;
-
-public interface LegacyCatalog {
-    // CONTRACT Assumes CAD as the currency
-    Option<Integer> findPrice(Barcode barcode);
-}
