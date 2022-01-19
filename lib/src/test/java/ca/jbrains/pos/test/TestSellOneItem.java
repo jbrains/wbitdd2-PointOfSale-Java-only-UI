6a1ed72f88ef039bbf998ac5eb7f2bcd5d0b0cde
J. B. Rainsberger <me@jbrains.ca>
2022-01-19 12:54:43 -0400

    Removed references to Basket from one of our test classes.

--- lib/src/test/java/ca/jbrains/pos/test/TestSellOneItem.java
+++ lib/src/test/java/ca/jbrains/pos/test/TestSellOneItem.java
@@ -2,8 +2,8 @@ package ca.jbrains.pos.test;
 
 import ca.jbrains.pos.Barcode;
 import ca.jbrains.pos.PointOfSale;
-import ca.jbrains.pos.domain.Basket;
 import ca.jbrains.pos.domain.Catalog;
+import ca.jbrains.pos.domain.PurchaseProvider;
 import io.vavr.control.Option;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -28,29 +28,52 @@ public class TestSellOneItem {
 
     @Test
     void priceFound() {
-        DoNothingBasket basket = new DoNothingBasket();
-        String response = PointOfSale.handleSellOneItemRequest(Barcode.makeBarcode("99999").get(), priceFoundCatalog, TestTotal.adaptBasketToPurchaseProvider(basket));
+        String response = PointOfSale.handleSellOneItemRequest(Barcode.makeBarcode("99999").get(), priceFoundCatalog, createDoNothingPurchaseProvider());
 
         Assertions.assertEquals("CAD 1.00", response);
     }
 
-    @Test
-    void addItemToBasketWhenProductIsFound() {
-        RecordingBasket basket = new RecordingBasket();
+    public static PurchaseProvider createDoNothingPurchaseProvider() {
+        return new PurchaseProvider() {
+            @Override
+            public void startPurchase() {
+            }
 
-        PointOfSale.handleSellOneItemRequest(Barcode.makeBarcode("::any barcode::").get(), priceFoundCatalog, TestTotal.adaptBasketToPurchaseProvider(basket));
-        Assertions.assertEquals(Option.some(100), basket.recentPrice);
+            @Override
+            public int getTotal() {
+                return 0;
+            }
+
+            @Override
+            public void addItem(int price) {
+            }
+        };
     }
 
-    private static class DoNothingBasket implements Basket {
+    @Test
+    void addItemToBasketWhenProductIsFound() {
+        RecordingPurchaseProvider purchaseProvider = new RecordingPurchaseProvider();
+
+        PointOfSale.handleSellOneItemRequest(Barcode.makeBarcode("::any barcode::").get(), priceFoundCatalog, purchaseProvider);
+
+        Assertions.assertEquals(Option.some(100), purchaseProvider.priceOfMostRecentItemAdded);
+    }
+
+    private static class RecordingPurchaseProvider implements PurchaseProvider {
+        public Option<Integer> priceOfMostRecentItemAdded = Option.none();
+
         @Override
-        public void add(int price) {
+        public void startPurchase() {
         }
 
         @Override
         public int getTotal() {
             return 0;
         }
-    }
 
+        @Override
+        public void addItem(int price) {
+            this.priceOfMostRecentItemAdded = Option.some(price);
+        }
+    }
 }
