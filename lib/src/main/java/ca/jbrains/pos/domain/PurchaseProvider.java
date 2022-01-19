c2e1b01eae1778de4e015ee92696a78dec059d37
J. B. Rainsberger <me@jbrains.ca>
2022-01-19 12:10:56 -0400

    Documented a small part of the contract.

--- lib/src/main/java/ca/jbrains/pos/domain/PurchaseProvider.java
+++ lib/src/main/java/ca/jbrains/pos/domain/PurchaseProvider.java
@@ -1,5 +1,8 @@
 package ca.jbrains.pos.domain;
 
+// CONTRACT
+// keeps track of whether a Purchase has been started
+// provides summary information (total cost) for Purchases
 public interface PurchaseProvider {
     void startPurchase();
 
