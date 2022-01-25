0a8000b2ccadcf7065c746d9d75e508b799a1830
chbndrhnns <chbndrhnns@users.noreply.github.com>
2022-01-25 14:26:41 +0100

    Removed a test that we no longer need

--- lib/src/test/java/ca/jbrains/pos/test/TestSellMultipleItems.java
+++ lib/src/test/java/ca/jbrains/pos/test/TestSellMultipleItems.java
@@ -1,66 +1,12 @@
 package ca.jbrains.pos.test;
 
-import ca.jbrains.pos.Barcode;
 import ca.jbrains.pos.PointOfSale;
-import ca.jbrains.pos.domain.Catalog;
 import ca.jbrains.pos.domain.PurchaseProvider;
-import io.vavr.control.Either;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class TestSellMultipleItems {
     private boolean startPurchaseInvoked = false;
-
-    @Disabled("wip")
-    @Test
-    void differentCustomersShouldHaveSeparateBaskets() {
-        // REFACTOR Maybe use Mockito to stub?
-        Catalog catalog = new Catalog() {
-            @Override
-            public Either<Barcode, Integer> findPrice(Barcode barcode) {
-                return new Barcode("12345").equals(barcode)
-                        ? Either.right(100)
-                        : Either.right(150);
-            }
-        };
-
-        PurchaseProvider purchaseProvider = new PurchaseProvider() {
-            @Override
-            public void startPurchase() {
-            }
-
-            public int getTotal() {
-                return -1;
-            }
-
-            @Override
-            public void addPriceOfScannedItem(int price) {
-
-            }
-        };
-        PointOfSale.handleBarcode(new Barcode("12345"), catalog, purchaseProvider);
-        PointOfSale.handleTotal(purchaseProvider);
-
-        PointOfSale.handleBarcode(new Barcode("67890"), catalog, purchaseProvider);
-        purchaseProvider = new PurchaseProvider() {
-            @Override
-            public void startPurchase() {
-            }
-
-            public int getTotal() {
-                return -1;
-            }
-
-            @Override
-            public void addPriceOfScannedItem(int price) {
-
-            }
-        };
-        PointOfSale.handleTotal(purchaseProvider);
-
-        Assertions.fail("PurchaseProvider needs to verify the items that were added");
-    }
 
     @Test
     void newPurchaseUsesNewBasket() {
