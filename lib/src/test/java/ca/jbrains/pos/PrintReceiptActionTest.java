21b9906b9059454c7defcdeb5571cda8db1cb76a
Johannes Rueschel <johannes.rueschel@telekom.de>
2022-02-15 13:48:04 +0100

    We can print a receipt, but only the total amount part, and only if there is a completed purchase
    mob next [ci-skip] [ci skip] [skip ci]
    
    mob next [ci-skip] [ci skip] [skip ci]

--- lib/src/test/java/ca/jbrains/pos/PrintReceiptActionTest.java
+++ lib/src/test/java/ca/jbrains/pos/PrintReceiptActionTest.java
@@ -1,5 +1,6 @@
 package ca.jbrains.pos;
 
+import ca.jbrains.pos.domain.PurchaseAccumulator;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -10,13 +11,29 @@ import static org.junit.jupiter.api.Assertions.*;
 class PrintReceiptActionTest {
     @Test
     void happyPathAfterOnly1Purchase() {
-        FormatMonetaryAmount formatMonetaryAmount = new FormatMonetaryAmount(Locale.ENGLISH);
-        final FormatTotal formatTotal = new FormatTotal(formatMonetaryAmount);
-        assertEquals("Total : 7.90 CAD", new PrintReceiptAction() {
+        final FormatTotal formatTotal = new FormatTotal(new FormatMonetaryAmount(Locale.ENGLISH));
+        PurchaseAccumulator purchaseAccumulator = new PurchaseAccumulator() {
+            @Override
+            public Purchase completePurchase() {
+                return new Purchase(790);
+            }
+
+            @Override
+            public void addPriceOfScannedItemToCurrentPurchase(int price) {
+
+            }
+        };
+        assertEquals("12345          CAD 7.90" +
+                System.lineSeparator() +
+                "Total: CAD 7.90", new PrintReceiptAction() {
             @Override
             public String printReceipt() {
-                return formatTotal.formatTotal(790);
+                return "12345          CAD 7.90" +
+                        System.lineSeparator() +
+                        formatTotal.formatTotal(purchaseAccumulator.completePurchase().total());
             }
         }.printReceipt());
     }
+
+
 }
